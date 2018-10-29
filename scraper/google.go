47cfe3df82d8da162964d2a6a4860ff83130da36
0x09AL <25826294+0x09AL@users.noreply.github.com>
2018-10-29 07:55:45 -0700

    Fixed URL extraction

--- scraper/google.go
+++ scraper/google.go
@@ -7,6 +7,23 @@ import (
 	"net/url"
 )
 
+// Taken from stackoverflow :p
+
+func RemoveDuplicatesFromSlice(s []string) []string {
+	m := make(map[string]bool)
+	for _, item := range s {
+		if _, ok := m[item]; ok {
+		} else {
+			m[item] = true
+		}
+	}
+	var result []string
+	for item, _ := range m {
+		result = append(result, item)
+	}
+	return result
+}
+
 func ExtractLinkedin(company string,state string,pagesNumber int,page agouti.Page) []string {
 	var URLs []string
 
@@ -18,19 +35,16 @@ func ExtractLinkedin(company string,state string,pagesNumber int,page agouti.Pag
 
 
 		page.Navigate(UrlDork)
-		UrlCount , _ := page.AllByClass(`f`).Count()
-
+		//UrlCount , _ := page.AllByClass(`f`).Count()
+		UrlCount , _ := page.AllByXPath(`//a[@href]`).Count()
 		for i:=0; i < UrlCount; i++ {
-			temp,_ := page.AllByClass(`f`).At(i).Text()
-
+			temp,_ := page.AllByXPath(`//a[@href]`).At(i).Text()
 			r := regexp.MustCompile(`https:\/\/([a-zA-Z]*)\.linkedin\.com([a-zA-Z0-9\/-]*)`)
 			URLs = append(URLs,r.FindString(temp))
 
 		}
 
 	}
-
-
-
+	URLs = RemoveDuplicatesFromSlice(URLs)
 	return URLs
 }
